a9ef77346358c3ec2cf59cfe5ea474042c5bde2b
grossm90 <grossm90@outlook.com>
2024-06-20 13:39:31 -0400

    Organize directory for express

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require ('express');
 
 const app = new express;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.sendFile(path.resolve(__dirname, 'public/pages/index.html'));
 });
 
 app.listen(4000, () => {
