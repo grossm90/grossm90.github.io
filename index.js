93e4ac06ed57950291535f067277649860753943
grossm90 <grossm90@outlook.com>
2024-06-20 14:44:48 -0400

    Add all pages to server directory

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const path = require ('path');
+const path = require('path');
 
-const express = require ('express');
+const express = require('express');
 
 const app = new express;
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public/pages/index.html'));
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/resume', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/resume.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/projects.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
 });
 
 app.listen(4000, () => {
