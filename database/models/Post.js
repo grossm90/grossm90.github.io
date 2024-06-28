7400b1da62acbb53ce592f1d6015047258a5e75f
grossm90 <grossm90@outlook.com>
2024-06-28 18:37:36 -0400

    Add 'Date Created' to posts

--- database/models/Post.js
+++ database/models/Post.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
-    content: String
+    content: String,
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 const Post = mongoose.model('Post', PostSchema);
